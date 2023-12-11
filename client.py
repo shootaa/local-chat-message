714a906814bfe5dbbd20fc2d859ce9c6a633eac9
清遠翔太 <shota@shinenshoutanoMacBook-Air-2.local>
2023-12-12 00:03:55 +0900

    feat: きめ打ちのメッセージが表示されるようにした。

--- client.py
+++ client.py
@@ -1,8 +1,41 @@
-message = input("サーバーに送りたいメッセージを入力してください")
+import socket
+import os
+
+# UNIXドメインソケットとデータグラム（非接続）ソケットを作成します
+sock = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
+
+# サーバのアドレスを定義します。
+# サーバはこのアドレスでメッセージを待ち受けます
+server_address = '/tmp/udp_socket_file'
+
+# このクライアントのアドレスを定義します。
+# サーバはこのアドレスにメッセージを返します
+address = '/tmp/hoge'
+
+# サーバに送信するメッセージを定義します
+message = b'Message to send to the client.'
+
+# このクライアントのアドレスをソケットに紐付けます。
+# これはUNIXドメインソケットの場合に限ります。
+# このアドレスは、サーバによって送信元アドレスとして受け取られます。
+if os.path.exists(address):
+    os.unlink(address)
+sock.bind(address)
 
 try:
-    print("{}というメッセージを送ります".format(message))
+    # サーバにメッセージを送信します
+    print('sending {!r}'.format(message))
+    sent = sock.sendto(message, server_address)
 
-    print("サーバーからのメッセージを受け取っています。")
+    # サーバからの応答を待ち受けます
+    print('waiting to receive')
+    # 最大4096バイトのデータを受け取ります
+    data, server = sock.recvfrom(4096)
+
+    # サーバから受け取ったメッセージを表示します
+    print('received {!r}'.format(data))
+
 finally:
-    print("ソケットを閉じます")
+    # 最後にソケットを閉じてリソースを解放します
+    print('closing socket')
+    sock.close()
